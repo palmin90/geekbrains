90c3e09bf4dd71c321d3de3ed78a889ecfea030c
palmin90 <palmin90@mail.ru>
2019-12-21 15:42:10 +0300

    sa

--- php2/l1/createTov.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class TovFactory {
-    function createGood($name, $price) {
-        return new Tovar($name,$price);
-    }
-}
--- php2/l1/materials/Outer.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Outer{
-    function test(){
-        echo "<hr>Привет из внешнего класса<hr>";
-    }
-}
